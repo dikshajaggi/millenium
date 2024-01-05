96dd8f402b83d30cd11609d69c630d47cd819d17
dikshajaggi <cee.diksha@gmail.com>
2024-01-05 16:47:14 +0530

    show/hide password functionality added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,85 +1,137 @@
 // LoginForm.js
-import React, { useContext } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { Link, useNavigate } from 'react-router-dom';
-import { MainContext } from '../context/MainContext';
-import "./styles.scss"
+import React, { useContext, useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Link, useNavigate } from "react-router-dom";
+import { MainContext } from "../context/MainContext";
+import "./styles.scss";
+import hide from "../assests/images/hide.png";
+import show from "../assests/images/visible.png";
 
 const LoginSchema = Yup.object().shape({
-    username: Yup.string().required('Username is required'),
-    password: Yup.string().required('Password is required'),
+  username: Yup.string().required("Username is required"),
+  password: Yup.string().required("Password is required"),
 });
 
 const Login = ({ onSubmit }) => {
-    const context = useContext(MainContext)
-    const navigate = useNavigate()
+  const context = useContext(MainContext);
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handleLogin = async (values) => {
-        console.log(values, "login")
-        try {
-            const response = await fetch("https://millenium-orthodontics.onrender.com/api/user/login", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(values),
-            })
-            const resJson = await response.json()
-            if (response.ok) {
-                localStorage.setItem('user', JSON.stringify({ name: values.username, token: resJson.token }))
-                context.setUser(values.username)
-                navigate("/")
-            }
-            console.log(resJson.token, resJson)
-            context.setUserLoginToken(resJson.token)
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-            // token needs to be in context as it will be required to add products to the cart
-
-        } catch (error) {
-            // Handle login error
-            console.error('Login failed:', error);
+  const handleLogin = async (values) => {
+    console.log(values, "login");
+    try {
+      const response = await fetch(
+        "https://millenium-orthodontics.onrender.com/api/user/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
         }
-    };
+      );
+      const resJson = await response.json();
+      if (response.ok) {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ name: values.username, token: resJson.token })
+        );
+        context.setUser(values.username);
+        navigate("/");
+      }
+      console.log(resJson.token, resJson);
+      context.setUserLoginToken(resJson.token);
 
-    return (
-        <>
-            {context.userLoginToken ? <div>already logged in</div> : <Formik
-                initialValues={{ username: '', password: '' }}
-                validationSchema={LoginSchema}
-                onSubmit={handleLogin}
-            >
-                <div className='d-flex justify-content-center align-items-center minheight'>
-                    <Form className='d-flex flex-column justify-content-evenly align-items-center border formheight'>
-                        <div className='text-capitalize'>
-                            <div className='d-flex justify-content-between align-items-center divWidth' >
-                                <label htmlFor="username" className='mr-10'>Username:</label>
-                                <Field type="text" id="username" name="username" />
-                            </div>
-                            <ErrorMessage name="username" component="div" className="text-danger" />
-                        </div>
+      // token needs to be in context as it will be required to add products to the cart
+    } catch (error) {
+      // Handle login error
+      console.error("Login failed:", error);
+    }
+  };
 
-                        <div className='text-capitalize'>
-                            <div className='d-flex justify-content-between align-items-center divWidth'>
-                                <label htmlFor="password">Password:</label>
-                                <Field type="password" id="password" name="password" />
-                            </div>
-                            <ErrorMessage name="password" component="div" className="text-danger" />
-                        </div>
-
-                        <div className='text-capitalize'>
-                            <button className="btn buttons btn-sm" type="submit">Login</button>
-                        </div>
-                        <div className='text-capitalize'>
-                            not registred?
-                            <Link to="/signup" className='linkclass'> <button className="btn buttons btn-sm" type="submit">Signup</button></Link>
-                        </div>
-                    </Form>
+  return (
+    <>
+      {context.userLoginToken ? (
+        <div>already logged in</div>
+      ) : (
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          validationSchema={LoginSchema}
+          onSubmit={handleLogin}
+        >
+          <div className="d-flex justify-content-center align-items-center minheight">
+            <Form className="d-flex flex-column justify-content-evenly align-items-center border formheight">
+              <div className="text-capitalize">
+                <div className="d-flex justify-content-between align-items-center divWidth">
+                  <label htmlFor="username" className="mr-10">
+                    Username:
+                  </label>
+                  <Field type="text" id="username" name="username" />
                 </div>
-            </Formik>}
-        </>
-    );
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              <div className="text-capitalize">
+                <div className="d-flex justify-content-between align-items-center divWidth">
+                  <label htmlFor="password">Password:</label>
+                  <Field
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    name="password"
+                  />
+                </div>
+                <button
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  style={{
+                    backgroundColor: "transparent",
+                    border: "none",
+                    position: "relative",
+                    left: 270,
+                  }}
+                >
+                  <img
+                    src={showPassword ? hide : show}
+                    style={{ height: "2.5vh" }}
+                    alt="show/hide pass"
+                  />
+                </button>
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              <div className="text-capitalize">
+                <button className="btn buttons btn-sm" type="submit">
+                  Login
+                </button>
+              </div>
+              <div className="text-capitalize">
+                not registred?
+                <Link to="/signup" className="linkclass">
+                  {" "}
+                  <button className="btn buttons btn-sm" type="submit">
+                    Signup
+                  </button>
+                </Link>
+              </div>
+            </Form>
+          </div>
+        </Formik>
+      )}
+    </>
+  );
 };
 
 export default Login;
-
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,81 +1,126 @@
 // SignupForm.js
-import React, { useContext } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { useNavigate } from 'react-router-dom';
-import { MainContext } from '../context/MainContext';
-import "./styles.scss"
-
+import React, { useContext, useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { MainContext } from "../context/MainContext";
+import "./styles.scss";
+import hide from "../assests/images/hide.png";
+import show from "../assests/images/visible.png";
 
 const SignupSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Email is required'),
-    username: Yup.string().required('Username is required'),
-    password: Yup.string().required('Password is required'),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  username: Yup.string().required("Username is required"),
+  password: Yup.string().required("Password is required"),
 });
 
-
 const Signup = ({ onSubmit }) => {
-    const navigate = useNavigate()
-    const context = useContext(MainContext)
+  const navigate = useNavigate();
+  const context = useContext(MainContext);
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handleSignup = async (values) => {
-        try {
-            const response = await fetch("https://millenium-orthodontics.onrender.com/api/user/register", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(values),
-            })
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-            if (response.ok) navigate("/login")
-        } catch (error) {
-            // Handle signup error
-            console.error('Signup failed:', error);
+  const handleSignup = async (values) => {
+    try {
+      const response = await fetch(
+        "https://millenium-orthodontics.onrender.com/api/user/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
         }
-    };
-    return (
-        <>
-            {context.userLoginToken ? <div>already logged in</div> : <Formik
-                initialValues={{ email: '', username: '', password: '' }}
-                validationSchema={SignupSchema}
-                onSubmit={handleSignup}
-            >
-                <div className='d-flex justify-content-center align-items-center minheight'>
-                    <Form className='d-flex flex-column justify-content-evenly align-items-center border formheight'>
-                        <div>
-                            <div className='d-flex justify-content-between align-items-center divWidth'>
-                                <label htmlFor="email">Email:</label>
-                                <Field type="email" id="email" name="email" />
-                            </div>
-                            <ErrorMessage name="email" component="div" className="text-danger" />
-                        </div>
+      );
 
-                        <div>
-                            <div className='d-flex justify-content-between align-items-center divWidth'>
-                                <label htmlFor="username">Username:</label>
-                                <Field type="text" id="username" name="username" />
-                            </div>
-                            <ErrorMessage name="username" component="div" className="text-danger" />
-                        </div>
-
-                        <div>
-                            <div className='d-flex justify-content-between align-items-center divWidth'>
-                                <label htmlFor="password">Password:</label>
-                                <Field type="password" id="password" name="password" />
-                            </div>
-                            <ErrorMessage name="password" component="div" className="text-danger" />
-                        </div>
-
-                        <div>
-                            <button type="submit" className="btn buttons btn-sm">Signup</button>
-                        </div>
-                    </Form>
+      if (response.ok) navigate("/login");
+    } catch (error) {
+      // Handle signup error
+      console.error("Signup failed:", error);
+    }
+  };
+  return (
+    <>
+      {context.userLoginToken ? (
+        <div>already logged in</div>
+      ) : (
+        <Formik
+          initialValues={{ email: "", username: "", password: "" }}
+          validationSchema={SignupSchema}
+          onSubmit={handleSignup}
+        >
+          <div className="d-flex justify-content-center align-items-center minheight">
+            <Form className="d-flex flex-column justify-content-evenly align-items-center border formheight">
+              <div>
+                <div className="d-flex justify-content-between align-items-center divWidth">
+                  <label htmlFor="email">Email:</label>
+                  <Field type="email" id="email" name="email" />
                 </div>
-            </Formik>}
-        </>
-    );
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              <div>
+                <div className="d-flex justify-content-between align-items-center divWidth">
+                  <label htmlFor="username">Username:</label>
+                  <Field type="text" id="username" name="username" />
+                </div>
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              <div>
+                <div className="d-flex justify-content-between align-items-center divWidth">
+                  <label htmlFor="password">Password:</label>
+                  <Field
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    name="password"
+                  />
+                </div>
+                <button
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  style={{
+                    backgroundColor: "transparent",
+                    border: "none",
+                    position: "relative",
+                    left: 270,
+                  }}
+                >
+                  <img
+                    src={showPassword ? hide : show}
+                    style={{ height: "2.5vh" }}
+                    alt="show/hide pass"
+                  />
+                </button>
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              <div>
+                <button type="submit" className="btn buttons btn-sm">
+                  Signup
+                </button>
+              </div>
+            </Form>
+          </div>
+        </Formik>
+      )}
+    </>
+  );
 };
 
 export default Signup;
-
