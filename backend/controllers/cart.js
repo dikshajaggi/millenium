53cc99f87f87c84900ed2803d62959c73c33b7e3
cee-diksha <cee.diksha@gmail.com>
2024-08-14 10:43:19 +0530

    add-to-cart done

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -1,5 +1,21 @@
-export const addToCart = (req, res) => {
+import userModel from "../models/user.js"
 
+export const addToCart = async(req, res) => {
+    try{
+        // req.body.userid -> we are getting this from the authMiddleware
+        const userData = await userModel.findOne({_id: req.body.userid})
+        console.log(req.body.userid, userData, "req.body.userid")
+
+        let cartData = await userData.cartData
+        if (!cartData[req.body.itemid])cartData[req.body.itemid] = 1
+        else cartData[req.body.itemid] += 1
+        await userModel.findByIdAndUpdate(req.body.userid, {cartData})
+        return res.json({success: true, message: "item added to cart"})
+    } catch(error){
+        console.log(error, "error check")
+        return res.json({success: false, message: "error"})
+    }
+   
 }
 
 export const removeFromCart = (req, res) => {
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,7 +4,6 @@ import validator from "validator"
 import userModel from "../models/user.js"
 
 export const login = async (req, res) => {
-    console.log(res.body, "checking bosyyy")
     const {password, email} = req.body
     try {
         const user = await userModel.findOne({email})
