29f1687c96ccfec99561f15e6ee7c45ef09cc6ca
Shubh-WebD <sharma.shubham2124@gmail.com>
2025-02-02 21:37:27 +0530

    static info changed

--- frontend/src/assests/static.js
+++ frontend/src/assests/static.js
@@ -1,7 +1,7 @@
 export const currentDate = "Sat Feb 03 2025"
 export const companyName = "Dwarka Orthodontics"
 export const contactAddress = "Dwarka, New Delhi, India"
-export const contactEmail = "intmillenium@gmail.com"
+export const contactEmail = "dwarkaorthodontics@gmail.com"
 export const contactNumber = "+91 9811704446"
 export const companyWebsiteUrl = "https://dwarkaorthodontics.com/"
 export const contactMessage = "Hello"; // replace with the desired message
@@ -9,4 +9,4 @@ export const whatsappUrl = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
     ? `https://wa.me/${contactNumber}?text=${encodeURIComponent(contactMessage)}`
     : `https://web.whatsapp.com/send?phone=${contactNumber}&text=${encodeURIComponent(contactMessage)}`;
 export const fbPageUrl = "https://www.facebook.com/millenium.orthodontics";
-export const instaPageUrl = "https://www.instagram.com/millenium_ortho?igsh=MWV6bTNobTBtN2xheQ==";
+export const instaPageUrl = "https://www.instagram.com/dwarkaorthodontics/?igsh=MWJydDdjdzU0ZmN2bg%3D%3D#";
